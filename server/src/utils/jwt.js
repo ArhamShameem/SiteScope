0e993fc39d3df716eb91f7fcf14155de025134b5
Arham Shameem <golsushameem001@gmail.com>
2026-04-05 23:13:47 +0530

    hot fix for prod

--- server/src/utils/jwt.js
+++ server/src/utils/jwt.js
@@ -20,7 +20,7 @@ function getCookieOptions() {
   return {
     httpOnly: true,
     secure: isProduction,
-    sameSite: 'lax',
+    sameSite: isProduction ? 'none' : 'lax',
     maxAge: 7 * 24 * 60 * 60 * 1000,
     path: '/',
   };
